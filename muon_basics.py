9293fb9986d71ea12ce7c19578328f4bcefad392
AJO-Python <48807287+AJO-Python@users.noreply.github.com>
2019-10-09 16:51:41 +0100

    Added decay rate and random sampling
    Random sample between 0-1 is converted to a particle lifetime according to PDF from decay rates

--- muon_basics.py
+++ muon_basics.py
@@ -10,7 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-
 # =============================================================================
 # Classes
 # =============================================================================
@@ -20,9 +19,10 @@ class muon:
         self.mass = 1.883531627e-28
         self.charge = 1
         self.mass_energy = 105.6583745e6
-        self.halflife = 2.2969811e-6
+        self.halflife = 2296.9811 # In nano seconds e-9
         self.spin = 0.5
         self.gamma_u = 2*np.pi*135.5e6
+        self.decay_const = np.log(2)/self.halflife
         
         self.pos = np.array(position, dtype="float64")
         self.vel = np.array(velocity, dtype="float64")
@@ -101,54 +101,51 @@ def get_unit_vec(vector):
     return vector / get_mag(vector)
 
 
-def decay(mu, time, dt):
-    decay_const = 1/mu.halflife
+def decay(mu, time):
+    decay_const = np.log(2)/mu.halflife
     decay_prob = decay_const * np.exp((-decay_const * time))
-    return decay_prob*dt
-
-    
+    return decay_prob
+"""
+def decay(decay_const, time):
+    decay_prob = 1 - np.exp(-decay_const * time)
+    return decay_prob
+"""
+def inv_decay(decay_const, U):
+    """
+    Inverse of the decay equation
+    Takes a number U={0, 1} and returns decay time
+    """
+    return -(np.log(U)) / U
 #%%
 # =============================================================================
 # Main
 # =============================================================================
-field = np.array(mag_field(500000, np.pi/2))
-
 # Creates random particles
 #particles = []
 #for i in range(1):
     #rand_pos, rand_vel = np.random.randint(-1000, 1000, size=(2, 3))
     #particles.append(muon(rand_pos, rand_vel))
 
+field = np.array(mag_field(500000, np.pi/2))
+
+
 # Setting up a standard muon
 pos = [2*np.pi, 0, 0]
-vel = [1.0, 0, 1.0]
+vel = [1.0, 1.0, 1.0]
 m1 = muon(pos, vel)
 m1.accel = muon_accel(m1, field)
 
 omega = larmor_freq(field, m1.gamma_u)
-
-
-
-
-plt.figure(3)
-dt = 4e-6/50
-decay_prob = []
-x_space = np.linspace(0, 4e-6, 50)
-for i in x_space:
-    decay_prob.append(decay(m1, i, dt))
-    #plt.scatter(i, decay_prob[-1])
-
-#plt.plot(x_space, decay_prob)
-n, bins, patches = plt.hist(decay_prob, bins=50, density=True, cumulative=True)
-plt.xlim(-1e-6, 5e-6)
+lifetime = inv_decay(m1.decay_const, np.random.rand())
 
 
 
 
 
+"""
 # Setting up simulation
-dt = 0.05
-count = 50
+dt = 0.1
+count = 10000
 N = int(count/dt)
 m1_pos = np.zeros([N, 3], dtype="float64")
 i=0
@@ -160,8 +157,10 @@ while i < N:
     
     m1_pos[i] = m1.pos
     i += 1
-
+"""
 #%%
+
+"""
 # =============================================================================
 # Plotting
 # =============================================================================
@@ -177,7 +176,7 @@ ax.plot3D(m1_pos[:,0], m1_pos[:,1], m1_pos[:,2],
 
 
 """
-Plotting the direction of the magnetic field
+#Plotting the direction of the magnetic field
 """
 x_min_max = [min(m1_pos[:,0]), max(m1_pos[:,0])]
 y_min_max = [min(m1_pos[:,1]), max(m1_pos[:,1])]
@@ -195,7 +194,7 @@ ax.plot3D([0, unit_field[0]*0.5*x_scale],
           linestyle="--", label="B_field")
 ax.legend(loc="best")
 
-
+"""
 
 
 
