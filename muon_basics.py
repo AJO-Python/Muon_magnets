c71cb07d4f4191b2ea1ffcb9a7e56ae8d24880cf
AJO-Python <48807287+AJO-Python@users.noreply.github.com>
2019-10-10 18:43:12 +0100

    Graphs and plot of precession affecting forward/backward detection

--- muon_basics.py
+++ muon_basics.py
@@ -18,7 +18,7 @@ class muon:
 
         self.mass = 1.883531627e-28
         self.charge = 1
-        self.mass_energy = 105.6583745e6
+        self.mass_energy = 105.6583745e6 # In MeV
         self.halflife = 2296.9811 # In nano seconds e-9
         self.spin = 0.5
         self.gamma_u = 2*np.pi*135.5e6
@@ -65,6 +65,7 @@ def asym(Nb, Nf):
 def mag_field(B0, theta):
     """
     Define magnetic fild in a plane X=theta and Y=phi
+    B0 in guass
     """
     return [0, B0*np.sin(theta), B0*np.cos(theta)]
 
@@ -132,30 +133,76 @@ def kubo_toyabe(t, w, theta):
     #rand_pos, rand_vel = np.random.randint(-1000, 1000, size=(2, 3))
     #particles.append(muon(rand_pos, rand_vel))
 
-field = np.array(mag_field(500000, 0))
+field = np.array(mag_field(1, 0))
 
 
 # Setting up a standard muon
-pos = [2*np.pi, 0, 0]
-vel = [1.0, 1.0, 1.0]
+pos = [2*np.pi, 200, 0.5]
+vel = [1.0, 0.0, 1.0]
 m1 = muon(pos, vel)
 m1.accel = muon_accel(m1, field)
 
 omega = larmor_freq(field, m1.gamma_u)
+forward, backward, for_time, back_time, both = list(), list(), list(), list(), list()
+for particle in range(int(2e5)):
+    lifetime = inv_decay(m1.decay_const, np.random.rand())
+    polarisation = kubo_toyabe(lifetime, omega, np.pi*2/3)
+    if polarisation >= 0:
+        forward.append(lifetime)
+        for_time.append(lifetime)
+    else:
+        backward.append(lifetime)
+        back_time.append(lifetime)
+    both.append(lifetime)
 
+#%%
 plt.figure()
-for j in np.linspace(0, 2*np.pi, 100):
+n_f, b_f, _ = plt.hist(forward, histtype="step",
+                       bins=1000, label="Forward", range=(0, 50))
+n_b, b_b, _ = plt.hist(backward, histtype="step",
+                       bins=1000, label="Backward", range=(0, 50))
+n_a, b_a, _ = plt.hist(both, histtype="step",
+                       bins=1000, label="Combined", range=(0, 50))
+#plt.yscale("log")
+plt.xlim(0, 10)
+plt.title("Histogram of particle lifetime (N=2e6, theta=2$\pi$3)")
+plt.xlabel("Lifetime (ns)")
+plt.ylabel("Frequency")
+plt.legend(loc="best")
+plt.grid()
+plt.figure()
+plt.plot(b_f[:-1], n_f, label="Forward")
+plt.plot(b_b[:-1], n_b, label="Backward")
+plt.plot(b_a[:-1], n_a, label="Both")
+plt.xlim(0, 10)
+plt.title("Plot of particles detected against time (N=2e5, theta=2$\pi$3)")
+plt.xlabel("Lifetime (ns)")
+plt.ylabel("Frequency")
+plt.legend(loc="best")
+plt.grid()
+#%%
+#==============================================================================
+# Precession of polarisation
+#==============================================================================
+line = list(["-", "-.", "--", "--", "-.", "--", "-", "-", "-.", "--", "-", "-.", "--"])
+theta_list = list([0, np.pi/3, np.pi/2, np.pi, np.pi*2/3, np.pi*2])
+#theta_list = list([0, np.pi/3, np.pi/2, np.pi])
+plt.figure()
+for i, theta in enumerate(theta_list):
     result = list()
-    for i in np.linspace(0, 1e-13, 1000):
-        result.append(kubo_toyabe(i, omega, j))
-
-    plt.plot(result)
-
+    for t in np.linspace(0, 0.2e-13, 1000):
+        result.append(kubo_toyabe(t, omega, theta))
+    plt.plot(result, label="{:.2f}$\pi$".format(theta/np.pi), linestyle=np.random.choice(line), alpha=1)
+plt.legend(loc="best")
+plt.title("Polarisation as a function of theta and time")
+plt.xlabel("Time (1e-9 s)")
+plt.ylabel("Polarisation ($\sigma$)")
+plt.grid()
 #%%
 #==============================================================================
 # 
 #==============================================================================
-"""
+
 # Setting up simulation
 dt = 0.1
 count = 10000
@@ -170,14 +217,14 @@ while i < N:
     
     m1_pos[i] = m1.pos
     i += 1
-"""
+
 #%%
 
-"""
+
 # =============================================================================
 # Plotting
 # =============================================================================
-fig = plt.figure(2)
+fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
@@ -205,9 +252,8 @@ ax.plot3D([0, unit_field[0]*0.5*x_scale],
           [0, unit_field[1]*0.5*y_scale],
           [0, unit_field[2]*0.5*z_scale],
           linestyle="--", label="B_field")
-ax.legend(loc="best")
-
-"""
+ax.legend(loc="lower right")
+plt.title("Single Muon moving in B-field with initial velocity in x, z")
 
 
 
