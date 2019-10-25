24d41e1df398c7a33f23e83cd007e19eb3b04cb2
Josh <josh.owen121@gmail.com>
2019-10-25 23:35:58 +0100

    Merged polarisation

--- Modules/muon.py
+++ Modules/muon.py
@@ -1,3 +1,5 @@
+import numpy as np
+import functions as func
 class Muon:
     def __init__(self):
         """
@@ -9,13 +11,16 @@ class Muon:
         self.halflife = 2.2969811e-6
         self.gamma_u = 2*np.pi*135.5e6
         self.decay_const = np.log(2)/self.halflife
-        self.inv_decay(np.random.rand())
-    
-    def inv_decay(self):
+        self.lifetime = float(self.inv_decay(np.random.rand(1)))
+        
+    def inv_decay(self, U):
         """
         Inverse of the decay equation
         Takes a number U={0, 1} and returns decay time
         """
-        U = np.random.rand(0, 1)
-        self.lifetime = -(np.log
-                          (U)) / U
+        return -(np.log(U)) / self.decay_const
+    
+    def get_spin_polarisation(self, field, theta):
+        w = func.larmor_freq(field, self.gamma_u)
+        t = self.lifetime
+        return np.cos(theta)**2 + (np.sin(theta)**2)*np.cos(w*t)
