f1eddaf2216f9cd62649d1a46926fa07a7faa883
Josh - chromebook <josh.owen121@gmail.com>
2020-02-20 21:19:14 +0000

    Changed all lines to <81 chars

--- Modules/dipole.py
+++ Modules/dipole.py
@@ -35,12 +35,14 @@ class Dipole(object):
         """Gets magnetic field at target location (x, y, z)"""
         # Check that coordinates are same dimension
         if not len(target)==len(self.location):
-            raise ValueError("Dimensions of target and self.location to not match")
-        mag_perm = 10**-7  # Cancel constant terms to get mag_perm as only constant
+            raise ValueError("Dimensions of arrays do not match")
+        mag_perm = 10**-7  # Cancel constant terms to get this
         relative_loc = np.subtract(np.array(target), self.location)
         magnitude = func.get_mag(relative_loc)
         return mag_perm * (
-                (3*relative_loc*(np.dot(self.moment, relative_loc)) / (magnitude**5))
+                (3*relative_loc*(np.dot(self.moment, relative_loc))
+                / (magnitude**5)
+                )
                 - (self.moment / (magnitude**3))
                 )
 
@@ -110,10 +112,13 @@ def set_field_values(x_array, y_array, dipole_grid, resolution=10):
     return field
 
 
-def create_dipole_grid(x_array, y_array, strength=1e-3, dipole_spacing=40, buffer=20, random_angle=False, angle=0, field_res=1):
+def create_dipole_grid(x_array, y_array, strength=1e-3,
+                       dipole_spacing=40, buffer=20,
+                       random_angle=False, angle=0, field_res=1):
     """
     Creates a grid of dipoles
-    Places a dipole every {dipole_spacing} on the x/y array with a buffer around the edges
+    Places a dipole every {dipole_spacing} on the x/y array with a buffer
+    around the edges
     """
     start = time.time()
     buffer = max(1, buffer)  # Ensure no zero or negative buffer values
@@ -126,7 +131,10 @@ def create_dipole_grid(x_array, y_array, strength=1e-3, dipole_spacing=40, buffe
             if random_angle:
                 angle = np.random.randint(0, 361)
             #print(i, j)
-            dipole_grid.change_point((i, j), Dipole(orientation=angle,location=[x, y, 0],strength=strength))
+            dipole_grid.change_point((i, j),
+                                    Dipole(orientation=angle,
+                                    location=[x, y, 0],
+                                    strength=strength))
     mid = time.time()
     print("Made {} dipoles in {:.3}s".format(Dipole.count, mid-start))
     Dipole.count = 0
@@ -135,7 +143,7 @@ def create_dipole_grid(x_array, y_array, strength=1e-3, dipole_spacing=40, buffe
 
 if __name__ == "__main__":
     Nx, Ny = 10, 10
-    side_length = 2e-3
+    side_length = 5
     x_array = np.linspace(-side_length, side_length, Nx)
     y_array = np.linspace(-side_length, side_length, Ny)
 
@@ -150,7 +158,6 @@ if __name__ == "__main__":
                                         strength=1e-2,
                                         buffer=1)
     field = set_field_values(x_array, y_array, dipoles_aligned, resolution=2)
-    #field = [dipoles_aligned.get_point(*coord) for coord in dipoles_aligned.all_coords()]
     print(field.get_axis_values("all"))
 
 """
