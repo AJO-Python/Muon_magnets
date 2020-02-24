Fixed naming issue with transverse/longitudinal methods for muons. Added working stream plot and verified that dipole fields are calculated correctly (tested by hand calculation) Organised /Images

Modules/dipole.py
@@ -3,11 +3,6 @@ import numpy as np
 import Modules.functions as func
 import Modules.grid as grid
 class Dipole(object):
-    """
-    Creates dipole:
-    location = (pos_x, pos_y)
-    orientation and length = pole_separation
-    """
     count = 0
     def __init__(self, orientation, location, strength):
         """
@@ -34,15 +29,16 @@ class Dipole(object):
     def get_mag_field(self, target):
         """Gets magnetic field at target location (x, y, z)"""
         # Check that coordinates are same dimension
-        if not len(target)==len(self.location):
+        if not len(target) == len(self.location):
             raise ValueError("Dimensions of target and self.location to not match")
-        mag_perm = 10**-7  # Cancel constant terms to get mag_perm as only constant
+        temp_moment = self.moment[:len(target)]
+        mag_perm = 10e-7  # Cancel constant terms to get mag_perm as only constant
         relative_loc = np.subtract(np.array(target), self.location)
         magnitude = func.get_mag(relative_loc)
         return mag_perm * (
-                (3*relative_loc*(np.dot(self.moment, relative_loc)) / (magnitude**5))
-                - (self.moment / (magnitude**3))
-                )
+                (3 * relative_loc * (np.dot(temp_moment, relative_loc)) / (magnitude ** 5))
+                - (temp_moment / (magnitude ** 3))
+        )
 
     def get_relative_loc(self, other):
         return other.location - self.location

__main__.py
@@ -5,19 +5,52 @@ import Modules.functions as func
 import Modules.muon as mu
 import Modules.dipole as dip
 import Modules.grid as grid
+import model_equations
 
-Nx, Ny = 10, 10
-side_length = 2e-3
-x_array = np.linspace(-side_length, side_length, Nx)
-y_array = np.linspace(-side_length, side_length, Ny)
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
-dipoles_aligned = dip.create_dipole_grid(x_array, y_array,
-                                         dipole_spacing=5,
-                                         random_angle=False,
-                                         strength=1e-2,
-                                         buffer=1)
-print("Test")
-print(dipoles_aligned)
-# field = set_field_values(x_array, y_array, dipoles_aligned, resolution=2)
-# field = [dipoles_aligned.get_point(*coord) for coord in dipoles_aligned.all_coords()]
-# print(field.get_axis_values("all"))
+dipole_1 = dip.Dipole(90, [0, 0], 1)
+dipole_2 = dip.Dipole(90, [1, 0], -1)
+dipole_3 = dip.Dipole(90, [1, 1], 1)
+dipole_4 = dip.Dipole(90, [0, 1], 1)
+dipoles = [dipole_1, dipole_2]  # , dipole_3, dipole_4]
+
+# Grid of x, y points
+nx, ny = 64, 64
+x = np.linspace(-2, 2, nx)
+y = np.linspace(-2, 2, ny)
+X, Y = np.meshgrid(x, y)
+# Electric field vector, E=(Ex, Ey), as separate components
+Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
+for i, x_ in enumerate(x):
+    for j, y_ in enumerate(y):
+        for d in dipoles:
+            ex, ey = d.get_mag_field(target=[x_, y_])
+            Ex[j][i] += ex
+            Ey[j][i] += ey
+            if i == 20 and j == 20:
+                print(x_, y_)
+                print(ex, ey)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+# Plot the streamlines with an appropriate colormap and arrow style
+color = 2 * np.log(np.hypot(Ex, Ey))
+ax.streamplot(x, y, Ex, Ey, color=color, linewidth=1, cmap=plt.cm.inferno,
+              density=2, arrowstyle='->', arrowsize=1.5)
+
+# Add filled circles for the charges themselves
+charge_colors = {True: '#aa0000', False: '#0000aa'}
+for d in dipoles:
+    ax.add_artist(Circle(d.location, 0.05, color=charge_colors[1 > 0]))
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+ax.set_aspect('equal')
+plt.savefig("Images/Dipoles/double_opp_dipole.png", bbox_inches="tight")
+plt.show()

model_equations.py
@@ -7,8 +7,6 @@ import Modules.dipole as dip
 
 mpl.rcParams["axes.formatter.limits"] = -2, 2  # Sets xticks to use exponents
 mpl.rcParams["axes.grid"] = True  # Turns grid on
-mpl.rcParams["legend.loc"] = "best"  # Turns legend on and autoplaces it
-
 
 def static_GKT(time, width):
     """
@@ -24,7 +22,7 @@ def static_GKT(time, width):
             )
 
 
-def transverse_GKT(time, width, ext_field_in_axis):
+def longitudinal_GKT(time, width, ext_field_in_axis):
     """
     :param object time: Length of time to calculate
     :param float width: Gaussian width parameter for internal field
@@ -46,11 +44,11 @@ def transverse_GKT(time, width, ext_field_in_axis):
 
     part_3 = np.zeros_like(time)
     for i, t in enumerate(time):
-        [part_3[i], _] = integrate.quad(transverse_integral, a=0, b=t, args=(omega, sigma))
+        [part_3[i], _] = integrate.quad(longitudinal_integral, a=0, b=t, args=(omega, sigma))
     return part_1 + (part_2 * part_3)
 
 
-def transverse_integral(time, omega, sigma):
+def longitudinal_integral(time, omega, sigma):
     """
     :param float time: Time to integrate at
     :param float omega: larmour precession frequency
@@ -82,25 +80,31 @@ def transverse_LKT(time, width, ext_field_in_axis):
     :param float ext_field_in_axis: External applied field
     :return: array
     """
-    omega = mu.Muon.gyro_ratio * ext_field_in_axis[2]
+    omega = mu.Muon.gyro_ratio * ext_field_in_axis
     sigma = mu.Muon.gyro_ratio * width
     sigma_t = (sigma ** 2) * (time ** 2)
 
 
-widths = np.logspace(-4, -3, 5, dtype=float)  # Tesla
-time = np.linspace(0, 20e-6, 100)
-output = np.zeros([len(widths), len(time)])
+if __name__ == "__main__":
+    widths = np.logspace(-4, -3, 5, dtype=float)  # Tesla
+    time = np.linspace(0, 20e-6, 150)
+    output = np.zeros([len(widths), len(time)])
+    av_data = np.zeros(len(time))
 
-for i, width in enumerate(widths):
-    output[i] = transverse_GKT(time, width, 1e-3)
+    for i, width in enumerate(widths):
+        output[i] = static_GKT(time, width)  # , 1e-3)
 
-plt.figure()
-for width, data in zip(widths, output):
-    plt.plot(time, data, label=f"Width: {width:.1e}T")
-plt.xlabel("Time (s)")
-plt.ylabel("Sum of muon precessions")
-plt.title("Transverse (z) Gaussian Kubo-Toyabe function ($G^{G}_{z}(t)$)")
-plt.savefig("Images/static_GKT.png", bbox_inches="tight")
-plt.legend(loc="best", fontsize="small")
+    plt.figure()
+    for width, data in zip(widths, output):
+        plt.plot(time, data, label=f"Width: {width:.1e}T")
+        # av_data = np.add(av_data, data)
+    # av_data = av_data/len(widths)
 
-plt.show()
+    # PLOTTING
+    plt.xlabel("Time (s)")
+    plt.ylabel("Sum of muon precessions (z-axis)")
+    plt.title("Zero field Gaussian Kubo-Toyabe function ($G^{G}_{z}(t)$)")
+    plt.legend(loc="best", fontsize="small")
+    # plt.plot(time, av_data, "r--", label="Average")
+    plt.show()
+    plt.savefig("Images/Analytical/ZF_GKT.png", bbox_inches="tight")