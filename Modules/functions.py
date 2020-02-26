7390405aa004844fac96c5e6416f70363b2e4bea
Josh - Desktop <josh.owen121@gmail.com>
2020-02-26 23:28:51 +0000

    Added functions to save and load objects as pickle files and arrays as txt files

--- Modules/functions.py
+++ Modules/functions.py
@@ -1,11 +1,13 @@
 import numpy as np
 
-gyro_ratio = 2*np.pi*135.5e6  # Radians per second per Tesla (rad s^-1 T^-1)
+gyro_ratio = 2 * np.pi * 135.5e6  # Radians per second per Tesla (rad s^-1 T^-1)
 halflife = 2.2969811e-6  # Seconds
-decay_const = np.log(2)/halflife  # Seconds
+decay_const = np.log(2) / halflife  # Seconds
 """
 VECTOR FUNCTIONS
 """
+
+
 def get_mag(vector):
     """
     Returns magnitude of 3-d vector
@@ -19,20 +21,23 @@ def get_unit_vector(vector):
     if not norm:
         return vector
     else:
-        return vector/norm
+        return vector / norm
 
 
 def get_angle(vec1, vec2):
     """
     Gets angle between vectors of shape (N,1) as theta {-pi2, pi/2}
     """
-    dot = sum(x*y for x,y in zip(vec1, vec2))
+    dot = sum(x * y for x, y in zip(vec1, vec2))
     mag1, mag2 = get_mag(vec1), get_mag(vec2)
-    return np.arccos(dot / (mag1*mag2))
+    return np.arccos(dot / (mag1 * mag2))
+
 
 """
 PARTICLE FUNCTIONS
 """
+
+
 def detect_asym(Nf, Nb):
     """
     Returns the asymmetry of the measurement
@@ -52,12 +57,14 @@ def detect_asym(Nf, Nb):
         else:
             return 1
 
+
 def format_plot(fig, max_time=20e-6):
     fig.legend(loc="best")
     fig.xlim(0, max_time)
     fig.grid()
     fig.ticklabel_format(axis="x", style="sci", scilimits=(-6, -6))
     fig.show()
+
 
 def chunk_muons(list_to_chunk, freq_per_chunk):
     """
@@ -69,6 +76,40 @@ def chunk_muons(list_to_chunk, freq_per_chunk):
     chunk_start = 0
     for i, freq in enumerate(freq_per_chunk):
         freq = int(freq)
-        chunks[i] = list_to_chunk[chunk_start:chunk_start+freq]
+        chunks[i] = list_to_chunk[chunk_start:chunk_start + freq]
         chunk_start += freq
     return chunks
+
+
+def save_array(filename, data):
+    """
+    :param str filename: Will save to "Muon_magnets/Results/{filename}.txt"
+    :param array data: 2d array to save to file
+    :return: Saves an array to a binary .npy file
+    """
+    file_path = f"./Results/{filename}.txt"
+    np.savetxt(file_path, data)
+    print(f"Saved to {file_path}")
+
+
+def save_object(filename, obj):
+    """
+    :param filename: Will save to "Muon_magnets/Results/{filename}.txt"
+    :param obj: Object to save
+    """
+    import pickle
+    file_path = f"./Results/{filename}.pickle"
+    with open(file_path, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+
+def load_object(filename):
+    """
+    :param filename: Will load from "Muon_magnets/Results/{filename}.pickle"
+    :rtype: object
+    :return: Object stored in file
+    """
+    import pickle
+    file_path = f"./Results/{filename}.pickle"
+    with open(file_path, 'rb') as output:  # Overwrites any existing file.
+        return pickle.load(output)
