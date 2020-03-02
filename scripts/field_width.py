1113c9e0b82be9c25ed3d34791b7b6d54c9ff32c
Josh - chromebook <josh.owen121@gmail.com>
2020-03-02 17:17:51 +0000

    Added saving and loading of muons so fields do not have to be recalculated

--- scripts/field_width.py
+++ scripts/field_width.py
@@ -3,8 +3,26 @@ import matplotlib.pyplot as plt
 
 from modules.muon import Muon
 from modules.dipole_grid import make_dipole_grid
-from modules.functions import load_run, get_mag
+from modules.functions import load_run, get_mag, save_array
 
+
+def spread_muons(run_name, N, dipole_array, max_loc):
+    """
+    Spreads N muons over the dipole grid and sets their attributes
+    to hold the sum of fields at the muons location
+    Saves the muon array to the run_name directory as "muons"
+
+    :param int N: Number of muons to model
+    :param array dipole_array: Dipoles spread over an area
+    :param tuple max_loc: Bounding region for muons
+    :return: saves array of muons with fields calculated from position on dipole grid
+    """
+    # Create and place muons
+    particles = {"muons": make_uniform_muons(N, max_loc)}
+    # Calculate field for each muon
+    set_muon_field(dipole_array, particles["muons"])
+    save_array(run_name, "muons", **particles)
+    return
 
 def make_uniform_muons(N, max_loc):
     """
@@ -27,46 +45,78 @@ def make_uniform_muons(N, max_loc):
 
 def set_muon_field(dipoles, muons):
     """
+    Calculates field experienced by each muon as a sum of contributions
+    from all the dipoles in the model
 
     :param array dipoles: array of dipoles
     :param array muons: array of muons
-    :return: muons set with their total field
+    :return: sets muon attributes to their total field
     """
     for i, dipole in enumerate(dipoles):
         print(f"On dipole {i}/{len(dipoles) - 1}")
         for muon in muons:
             # Checks if muon is inside a nano-island
-            if (
-                    ((muon.location[0] <= dipole.location[0] + 350e-9)
-                     and (muon.location[0] >= dipole.location[0] - 350e-9))
-                    or ((muon.location[1] >= dipole.location[1] + 8e-7)
-                        and (muon.location[1] <= dipole.location[1] - 8e-7))
-            ):
+            if is_inside_island(dipole, muon):
                 # TODO Add behaviour for inside nano-island
+                #muon.field += dipole.get_internal_field(muon.location)
                 continue
-            muon.field += dipole.get_mag_field(muon.location)
+            else:
+                muon.field += dipole.get_mag_field(muon.location)
+    return
+
+def is_inside_island(dipole, muon):
+    """
+    :param object dipole: Dipole to check
+    :param object muon: Muon to check is inside dipole
+    :rtype: bool
+    :return: True if muon is inside dipole
+    """
+    # Check if muon is inside x_coords
+    if ( (muon.location[0] <= dipole.location[0] + 350e-9)
+        and (muon.location[0] >= dipole.location[0] - 350e-9)):
+        inside_x = True
+    else:
+        return False  # Early exit condition
+
+    # Check if muon is inside y_coords
+    if ( (muon.location[1] <= dipole.location[1] + 8e-7)
+        and (muon.location[1] >= dipole.location[1] - 8e-7)):
+        return inside_x
+    else:
+        return False
 
 
-# Make dipole grid
-run_name = make_dipole_grid()
 
-# Load dipole grid
-dipole_data = load_run(run_name, files=["dipoles"])[0]
-dipole_array = dipole_data["dipoles"]
-max_loc = dipole_array[-1].location
+if __name__=="__main__":
+    """
+    Create dipole grid and scatter muons over it
+    Gets field widths and values
+    """
+    N = 10_000
 
-# Create and place muons
-N = 10_000
-muons = make_uniform_muons(N, max_loc)
-# Calculate field for each muon
-set_muon_field(dipole_array, muons)
+    # Make dipole grid
+    #run_name = make_dipole_grid()
 
-field_strengths = np.zeros_like(muons)
-for i, muon in enumerate(muons):
-    field_strengths[i] = get_mag(muon.field)
+    # Load dipole grid
+    dipole_data = load_run(run_name, files=["dipoles"])[0]
+    dipole_array = dipole_data["dipoles"]
+    max_loc = dipole_array[-1].location
 
-plt.figure()
+    # Spread muons over dipoles and calculate field values
+    #spread_muons(run_name=run_name, N=N, dipole_array=dipole_array, max_loc=max_loc)
+    muons_data = load_run(run_name, files=["muons"])[0]
+    muons = muons_data["muons"]
+    field_strengths = np.zeros_like(muons)
+    for i, muon in enumerate(muons):
+        field_strengths[i] = get_mag(muon.field)
 
-plt.hist(field_strengths, bins=100, range=(0, 2e9))
 
-plt.show()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(311)
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
+    ax1.hist(field_strengths, bins=100, label="field magnitude")
+    ax2.hist([m.location[0] for m in muons], bins=100, label="x field")
+    ax3.hist([m.location[1] for m in muons], bins=100, label="y field")
+    fig.legend(loc="best")
+    fig.show()
