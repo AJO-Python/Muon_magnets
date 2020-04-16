WIP: fix field calculation and distributions

__main__.py
@@ -1,12 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import time
+
 
 import modules.functions as func
+from modules.muon import Muon
 from modules.grid import Grid
 from modules.ensemble import Ensemble
-from modules.
+from modules.multi_process import MP_fields
+from modules.model_equations import static_GKT
+
 
 #########################################
-print("Loading config file 'dipole_array_config.txt'...")
-run_name = make_dipole_grid()
-print(f"Dipoles stored in: data/{run_name}")
+LOAD_OBJECTS = False
+CALCULATE = False
+RUN_NAME = "15X15_R_1"
+
+if LOAD_OBJECTS: # Load from file
+    island_grid = Grid(run_name=RUN_NAME, load_only=True)
+    particles = Ensemble(run_name=island_grid.run_name, load_only=True)
+
+else: # Calculate and save grid and ensemble
+    NUM_MUONS = 20000
+    island_grid = Grid()
+    RUN_NAME = island_grid.run_name
+    particles = Ensemble(N=NUM_MUONS, run_name=RUN_NAME)
+
+if CALCULATE:
+    print("Starting multiprocessing...")
+    start = time.time()
+    MP_fields(RUN_NAME, particles.muons, island_grid.islands)
+    end = time.time()
+    print(f"Time taken: {end - start}")
+
+fields, field_dict = func.load_fields(RUN_NAME)
+# Get each muons polarisation
+particles.set_relaxations(fields)
+# Normalise sum
+overall = np.nansum(particles.relaxations, axis=0) / particles.N
+
+# CURVE FIT
+popt, pcov = curve_fit(static_GKT, Muon.TIME_SCALE, overall, p0=1e-4)
+
+
+# Setup subplots
+ax0 = plt.subplot2grid((2, 3), (0, 0), colspan=2)
+ax1 = plt.subplot2grid((2, 3), (0, 2))
+ax2 = plt.subplot2grid((2, 3), (1, 0))
+ax3 = plt.subplot2grid((2, 3), (1, 1))
+ax4 = plt.subplot2grid((2, 3), (1, 2))
+
+field_axes = (ax1, ax2, ax3, ax4)
+# Plot individual lines if N is small
+if len(particles.relaxations) < 100:
+    for i in range(N):
+        ax0.plot(Muon.TIME_SCALE, particles.relaxations[i], alpha=0.5, lw=0.5)
+
+# Plot overall relaxation
+ax0.plot(Muon.TIME_SCALE, overall, lw=2, c="k", alpha=0.7, label="Model")
+ax0.plot(Muon.TIME_SCALE, static_GKT(Muon.TIME_SCALE, *popt), c="r", label="Curve fit")
+
+ax0.legend(loc="upper right")
+ax0.set_xlim(0, Muon.TIME_SCALE[-1])
+ax0.grid()
+ax0.set_title("Relaxation function from dipole grid")
+ax0.ticklabel_format(style="sci", axis="x", scilimits=(-6, -6))
+
+ax1.set_title("Magnitudes of overall field")
+
+for sub_ax, field in zip(field_axes, field_dict.keys()):
+    sub_ax.hist(field_dict[field], bins=100)
+    sub_ax.set_title(f"Magnitudes of {field}")
+    sub_ax.set_xlabel("Field strength (T)")
+    sub_ax.set_ylabel("Frequency")
+    sub_ax.grid()
+    sub_ax.ticklabel_format(style="sci", axis="x", scilimits=(-3, -3))
+# Add legend
+plt.tight_layout(pad=1)
+
+# print(f"Actual width: {random_width}")
+print(f"Calculated width: {popt} +- {pcov}")
+
+plt.show()

modules/ensemble.py
@@ -7,7 +7,7 @@ from modules.muon import Muon
 
 class Ensemble():
 
-    def __init__(self, N, guass_width=10e-6, run_name="", load_only=False):
+    def __init__(self, N=10_000, guass_width=10e-6, run_name="", load_only=False):
 
         self.run_name = run_name
 
@@ -20,9 +20,9 @@ class Ensemble():
         self.muons = np.array([Muon(loc=self.locations[i]) for i in range(self.N)])
         self.save_ensemble()
 
-    def create_locations(self):
+    def create_locations(self, z_dist=100e-6):
         locations = np.random.normal(loc=0, scale=self.width, size=(self.N, 2))
-        self.locations = np.insert(locations, 2, 0, axis=1)
+        self.locations = np.insert(locations, 2, z_dist, axis=1)
 
     def filter_in_dipoles(self, grid):
         pass
@@ -33,6 +33,9 @@ class Ensemble():
     def loader(self, run_name):
         params = func.load_object(run_name, "ensemble_obj")
         self.__dict__.update(params)
+
+    def set_relaxations(self, fields):
+        self.relaxations = np.array([p.full_relaxation(fields[i], decay=False) for i, p in enumerate(self.muons)])
 
     def show_on_plot(self, fig=None, ax=None):
         if not fig and not ax:

modules/multi_process.py
@@ -63,4 +63,5 @@ def MP_fields(folder_name, particles, dipoles,
     print("Saving data to file...")
     results = np.concatenate(result_list, axis=0)
     func.save_array(folder_name, "muon_fields", muon_fields=results)
-    print("Finished.")
+    print("Finished multiprocessing")
+    print("========")

modules/muon.py
@@ -142,7 +142,7 @@ class Muon:
             # Catch error if this is first field muon "feels"
             self.field = dipole.get_mag_field(self.loc)
 
-    def full_relaxation(self, field, life_limit=True):
+    def full_relaxation(self, field, decay=True):
         """
         Returns the polarisation of the muon against time
 
@@ -156,7 +156,7 @@ class Muon:
         y = Muon.GYRO_RATIO
         polarisation = np.cos(theta) ** 2 + (np.sin(theta) ** 2) * np.cos(y * H * t)
         # Set polarisation to zero if muon has decayed
-        if life_limit:
+        if decay:
             polarisation = np.where(Muon.TIME_SCALE < self.lifetime, polarisation, np.nan)
         return polarisation
 

scripts/relaxation.py
@@ -27,7 +27,7 @@ magnitudes = np.array([func.get_mag(f) for f in fields])
 field_dict = {"total": magnitudes, "x": field_x, "y": field_y, "z": field_z}
 
 # Get each muons polarisation
-relaxations = np.array([p.full_relaxation(fields[i], life_limit=False) for i, p in enumerate(particles)])
+relaxations = np.array([p.full_relaxation(fields[i], decay=False) for i, p in enumerate(particles)])
 # Normalise sum
 overall = np.nansum(relaxations, axis=0) / N
 