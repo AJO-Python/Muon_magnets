Added argument to set limits in plotting methods. Split dipole field calculation into parts to verify correct result

modules/dipole.py
@@ -50,15 +50,17 @@ class Dipole():
         if not len(target) == len(self.location):
             raise ValueError("Dimensions of target and self.location to not match")
         temp_moment = self.moment[:len(target)]  # Ensure moment is correct dimension
-        mag_perm = 1.257e-6  # Cancel constant terms to get mag_perm as only constant
+        mag_perm = 1e-7  # Cancel constant terms to get mag_perm as only constant
         relative_loc = np.subtract(np.array(target), self.location)
         magnitude = func.get_mag(relative_loc)
-
-        field = np.array(mag_perm * (
-                (3 * relative_loc * (np.dot(temp_moment, relative_loc))
-                 / (magnitude ** 5))
-                - (temp_moment / (magnitude ** 3))
-        ))
+        m_dot_r = np.dot(temp_moment, relative_loc)
+        field1 = np.array(
+            ((3 * relative_loc * m_dot_r)
+             / (magnitude ** 5)
+             ))
+        field2 = np.array(temp_moment / (magnitude ** 3))
+        field = np.subtract(field1, field2)
+        field *= mag_perm
         return field
 
     def get_relative_loc(self, other):

modules/grid.py
@@ -119,14 +119,15 @@ class Grid:
         self.patches = np.array([(isle.get_outline(), isle.get_moment_arrow()) for isle in self.islands]).flatten()
         self.collection = matplotlib.collections.PatchCollection(self.patches)
 
-    def show_on_plot(self, fig=None, ax=None):
+    def show_on_plot(self, fig=None, ax=None, set_lims=True):
         if not fig and not ax:
-            fig, ax = plt.subplots()
+            fig, ax = plt.subplots(figsize=func.set_fig_size(width="muon_paper"))
         self.make_collection()
         ax.add_collection(self.collection)
-        ax.set_xlim(func.get_limits(self.locations_x))
-        ax.set_ylim(func.get_limits(self.locations_y))
-        ax.set_aspect("equal")
+        if set_lims:
+            ax.set_xlim(func.get_limits(self.locations_x))
+            ax.set_ylim(func.get_limits(self.locations_y))
+            ax.set_aspect("equal")
         return fig, ax
 
     def save_config(self):